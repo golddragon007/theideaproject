ae834a2dc71a7e7886c9131fa92f079ee1fe52ff
Viktor Bán <viktor.ban@gmail.com>
2017-12-03 10:48:28 +0100

    Startup fixes

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker-compose up -d
-docker-compose exec php sh -c 'cd /var/www/html && ./init.sh'
+docker-compose exec php sh -c 'cd /var/www/html && ./wait-for-it.sh -t 60 mariadb:3306 -- ./init.sh'
